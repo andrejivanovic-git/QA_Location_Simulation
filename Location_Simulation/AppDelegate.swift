425ecfbc357d6f96225e782ddfbb6bba9e71c6ec
andrejivanovic-git <61945105+andrejivanovic-git@users.noreply.github.com>
2020-06-09 14:47:04 +0200

    Location Simulation

---
+++ Location_Simulation/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Location_Simulation
+//
+//  Created by Andrej Ivanović on 09/06/2020.
+//  Copyright © 2020 Andrej Ivanović. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
---
+++ Location_SimulationTests/Location_SimulationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Location_SimulationTests.swift
+//  Location_SimulationTests
+//
+//  Created by Andrej Ivanović on 09/06/2020.
+//  Copyright © 2020 Andrej Ivanović. All rights reserved.
+//
+
+import XCTest
+@testable import Location_Simulation
+
+class Location_SimulationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
